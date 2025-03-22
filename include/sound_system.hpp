f8aaffb466d41567d1b3b5cb6988219a8cafe4a4
dayshaunlee <leedayshaun@gmail.com>
2025-03-22 15:59:02 -0700

    fix crash fix

--- include/sound_system.hpp
+++ include/sound_system.hpp
@@ -13,19 +13,19 @@
 
 // System for loading and playing sounds
 class SoundSystem {
-    const std::array<std::string, sound_count> sound_paths_repeating = {
+    const std::array<std::string, 2> sound_paths_repeating = {
         audio_path("music.wav"),
-        audio_path("enemy_incoming.wav"),
+        audio_path("music.wav"),
     };
-    const std::array<std::string, sound_count> sound_paths_chunk = {
+    const std::array<std::string, 5> sound_paths_chunk = {
         audio_path("island-ship_collision.wav"),
         audio_path("ship-enemy_collision.wav"),
         audio_path("projectile-enemy_collision.wav"),
         audio_path("projectile-jail_collision.wav"),
         audio_path("game_over.wav")};
 
-    std::array<Mix_Music*, sound_count> sound_mix_repeating;
-    std::array<Mix_Chunk*, sound_count> sound_mix_chunk;
+    std::array<Mix_Music*, 2> sound_mix_repeating;
+    std::array<Mix_Chunk*, 5> sound_mix_chunk;
    public:
 
     bool init();
