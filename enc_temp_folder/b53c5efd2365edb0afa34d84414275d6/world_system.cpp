5f4378f576fcb79e734f4192f6598a4fd61dd374
Chinmay Bhansali <chinmaybhansali2001@gmail.com>
2025-03-22 22:31:09 -0700

    window size bug fix

--- enc_temp_folder/b53c5efd2365edb0afa34d84414275d6/world_system.cpp
+++ enc_temp_folder/b53c5efd2365edb0afa34d84414275d6/world_system.cpp
@@ -21,8 +21,6 @@ WorldSystem::WorldSystem() {
     // seeding rng with random device
     rng = std::default_random_engine(std::random_device()());
     fpsCounter = 0;
-    window_width_px = WINDOW_WIDTH_PX;
-    window_height_px = WINDOW_HEIGHT_PX;
 }
 
 WorldSystem::~WorldSystem() {
@@ -173,11 +171,6 @@ void WorldSystem::init(RenderSystem* renderer_arg) {
 bool WorldSystem::step(float elapsed_ms_since_last_update) {
     int current_width, current_height;
     glfwGetWindowSize(window, &current_width, &current_height);
-    if (current_width != window_width_px || current_height != window_height_px) {
-        window_width_px = current_width;
-        window_height_px = current_height;
-        std::cout << "Window size updated: " << window_width_px << "x" << window_height_px << std::endl;
-    }
 
     std::string title = "Bnuuy's Ship      FPS: " + std::to_string(fpsCounter) + "        " + title_points;
     glfwSetWindowTitle(window, title.c_str());
