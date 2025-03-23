a5bb487f5d017dd4a5cfad0955c09095421689ad
Clare <clarepan0@gmail.com>
2025-03-22 17:03:50 -0700

    add bubble mod to gacha pool

--- src/scenes/level_04.cpp
+++ src/scenes/level_04.cpp
@@ -14,7 +14,7 @@ Level04::Level04(WorldSystem* world_system, std::string map_filename, TEXTURE_AS
     this->bunnies_to_win = 0;
     this->texture = texture;
     GachaSystem::getInstance().setLevelPool(
-        4, {MODULE_TYPES::SIMPLE_CANNON, MODULE_TYPES::PLATFORM, MODULE_TYPES::LASER_WEAPON});
+        4, {MODULE_TYPES::SIMPLE_CANNON, MODULE_TYPES::PLATFORM, MODULE_TYPES::LASER_WEAPON, MODULE_TYPES::BUBBLE_MOD});
 }
 
 Level04::~Level04() {
